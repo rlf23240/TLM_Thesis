1f732b37e536951204fbb67a2433d2d7a8a9990d
AsheeHuang <tim956956a@gmail.com>
2019-08-04 17:02:16 +0800

    data F

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 int main() {
-    vector<string> data_sets{"A"};
+    vector<string> data_sets{"F"};
     vector<double> times{};
     clock_t start;
     for(const string &data_set : data_sets) {
@@ -17,8 +17,7 @@ int main() {
 
         Dantzig_wolfe dantzig_wolfe = Dantzig_wolfe(CargoRoute(data_set));
         dantzig_wolfe.output_result("Result_DW_" + data_set
-        + "_iter" + to_string(MAX_BP_ITER) + "_thres"
-        + oss.str(), double(clock() - start));
+        , double(clock() - start));
     }
     return 0;
 }
